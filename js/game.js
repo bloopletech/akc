4d61601d803a9b1441c51e3fdcccc9ef2859702a
Brenton "B-Train" Fletcher <i@bloople.net>
2018-06-08 00:58:02 +1000

    Rearrange files

--- js/game.js
+++ js/game.js
@@ -1,32 +1,137 @@
-var $;
-var engine;
-function init() {
-  $ = document.querySelector.bind(document);
+var Game = function() {
+  var DIRECTIONS = ["left", "up", "right", "down"];
+  var PATTERN_LENGTH = 6;
+  var MAX_DUPE_LENGTH = 2;
 
-  engine = new Engine(function() {});
-
-  if(window.innerWidth >= 460) {
-    window.addEventListener("keydown", function(event) {
-      engine.onKeyDown(event);
-    });
-    window.addEventListener("keyup", function(event) {
-      engine.onKeyUp(event);
-    });
-  }
-  else {
-    window.addEventListener("touchstart", function(event) {
-      engine.onTouchStart(event);
-    });
-    window.addEventListener("touchend", function(event) {
-      engine.onTouchEnd(event);
-    });
+  function randomDirection() {
+    return DIRECTIONS[Math.floor(Math.random() * DIRECTIONS.length)];;
   }
 
-  document.body.addEventListener("click", function(e) {
-    if(e.target.matches(".play")) engine.start();
-    else if(e.target.matches("#settings")) showBack;
-    else if(e.target.matches("#done")) showFront;
-  })
-}
+  function generatePattern() {
+    var pattern = null;
 
-document.addEventListener("DOMContentLoaded", init);
+    do {
+      pattern = [];
+      for(var i = 0; i < PATTERN_LENGTH; i++) pattern.push(randomDirection());
+
+      var dupes = false;
+      for(var i = MAX_DUPE_LENGTH; i < pattern.length; i++) {
+        var sliced = pattern.slice(i - MAX_DUPE_LENGTH, i + 1);
+
+        dupes = sliced.every(function(value) {
+          return value == sliced[0];
+        });
+
+        if(dupes) break;
+      }
+    }
+    while(dupes);
+
+    return pattern;
+  }
+
+  var allowedTime = 1200;
+  var score = 0;
+  var streak = 0;
+  var startTime = null;
+  var grinding = false;
+  var grindStart = null;
+  var grindDuration = 0;
+  var direction = null;
+  var pattern = generatePattern();
+  var correct = false;
+  var highPrecisionTimer = (typeof window.performance == "object");
+
+  function now() {
+    return highPrecisionTimer ? Math.round(performance.now()) : Date.now();
+  }
+
+  function timePassed() {
+    if(grindStart) {
+      return ((now() - grindStart) * 1.5) + (grindStart - startTime);
+    }
+    else {
+      return now() - startTime;
+    }
+  }
+
+  function timeRemaining() {
+    var time = (allowedTime - timePassed()) / (allowedTime + 0.0);
+    if(time < 0) return 0;
+    if(time > 1) return 1;
+    return time;
+  }
+
+  function stack() {
+    return streak % pattern.length;
+  }
+
+  function maxStacks() {
+    return stack() == (pattern.length - 1);
+  }
+
+  function roundStarted() {
+    direction = pattern[stack()];
+    startTime = now();
+    correct = false;
+    grinding = false;
+    grindStart = null;
+    grindDuration = 0;
+    return direction;
+  }
+
+  function input(playerDirection) {
+    correct = playerDirection == direction;
+    return correct;
+  }
+
+  function grindStarted() {
+    grindStart = now();
+    grinding = true;
+  }
+
+  function grindEnded() {
+    grindDuration += now() - grindStart;
+    grinding = false;
+  }
+
+  function roundEnded() {
+    var diff = Math.floor(timePassed());
+    if(diff < 50 || diff > allowedTime || !correct) return true;
+
+    streak++;
+    var delta = (allowedTime - diff) + (grindDuration * 5) + (streak * 100);
+    if(diff <= (allowedTime * 0.3)) delta *= 2;
+    score += delta;
+
+    if(stack() == 0) {
+      if(allowedTime >= 750) allowedTime -= 50;
+      else if(allowedTime > 300) allowedTime -= 30;
+    }
+
+    return false;
+  }
+
+  return {
+    allowedTime: function() {
+      return allowedTime;
+    },
+    grinding: function() {
+      return grinding;
+    },
+    score: function() {
+      return score;
+    },
+    streak: function() {
+      return streak;
+    },
+    stack: stack,
+    maxStacks: maxStacks,
+    roundStarted: roundStarted,
+    input: input,
+    timeRemaining: timeRemaining,
+    grindStarted: grindStarted,
+    grindEnded: grindEnded,
+    roundEnded: roundEnded
+  };
+};
