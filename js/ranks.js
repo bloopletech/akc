f40a1d117a1e7bf1ec4de52523f3baad858ce84e
Brenton "B-Train" Fletcher <i@bloople.net>
2018-06-18 18:29:26 +1000

    Extract ranks into separate file

---
+++ js/ranks.js
@@ -0,0 +1,53 @@
+var Ranks = (function() {
+  var RANKS = {
+    bronze: {
+      minScore: 0,
+      humanName: "Bronze",
+      class: "bronze"
+    },
+    silver: {
+      minScore: 50000,
+      humanName: "Silver",
+      class: "silver"
+    },
+    gold: {
+      minScore: 80000,
+      humanName: "Gold",
+      class: "gold"
+    },
+    platinum: {
+      minScore: 100000,
+      humanName: "Platinum",
+      class: "platinum"
+    },
+    diamond: {
+      minScore: 1100000,
+      humanName: "Diamond",
+      class: "diamond"
+    },
+    vanadium: {
+      minScore: 120000,
+      humanName: "Vanadium",
+      class: "vanadium"
+    },
+    strontium: {
+      minScore: 130000,
+      humanName: "Strontium",
+      class: "strontium"
+    }
+  };
+
+  function scoreRank(score) {
+    var currentRank = null;
+    for(var i in RANKS) {
+      var rank = RANKS[i];
+      if(score >= rank.minScore) currentRank = rank;
+    }
+
+    return currentRank;
+  }
+
+  return {
+    scoreRank: scoreRank
+  }
+})();
