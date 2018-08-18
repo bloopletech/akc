4b3269bfef92a2767abb71fec600341041a07caa
Brenton "B-Train" Fletcher <i@bloople.net>
2018-08-18 18:09:46 +1000

    Skip registering service worker in development

--- akc/js/swr.js
+++ akc/js/swr.js
@@ -22,6 +22,8 @@ if ('serviceWorker' in navigator) {
   // precaching requests don't degrade the first visit experience.
   // See https://developers.google.com/web/fundamentals/instant-and-offline/service-worker/registration
   window.addEventListener('load', function() {
+    if(location.host != "akc.link" && location.host != "akc.dokku.bloople.net") return;
+
     // Your service-worker.js *must* be located at the top-level directory relative to your site.
     // It won't be able to control pages unless it's located at the same level or higher than them.
     // *Don't* register service worker file in, e.g., a scripts/ sub-directory!
