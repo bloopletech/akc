41505e5c210539af0c2f99fa595384d05d58371f
Brenton "B-Train" Fletcher <i@bloople.net>
2018-09-14 11:25:23 +1000

    Buff pattern length (7 -> 8)

--- akc/js/game.js
+++ akc/js/game.js
@@ -38,7 +38,7 @@ window.Game = function(touch) {
   var cycles = 0;
   var direction = null;
   var pattern = [];
-  addDirections(pattern, 7);
+  addDirections(pattern, 8);
   var correct = false;
 
   function nextDirection() {
