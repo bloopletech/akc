a34fa7a9f62fd2c0292e11265d6ce6fbe647d809
Brenton "B-Train" Fletcher <i@bloople.net>
2018-09-26 02:18:09 +1000

    Give a proper reward for achieving boost, as it should be quite difficult now

--- akc/js/game.js
+++ akc/js/game.js
@@ -142,7 +142,7 @@ window.Game = function(touch) {
 
     var delta = (initialAllowedTime - reactionTime()) + (grindDuration(now) * 3) + (streak * 10);
     if(isFlame(now)) delta += 1000;
-    if(isBoost(now)) delta += 1000;
+    if(isBoost(now)) delta *= 2;
     if(combo) delta *= 2;
     return Math.floor(delta);
   }
