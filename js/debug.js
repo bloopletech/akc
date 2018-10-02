da3edfb726c474f4e12238843d216279b976c158
Brenton "B-Train" Fletcher <i@bloople.net>
2018-10-02 18:57:05 +1000

    Add basic script to support debugging during development

---
+++ js/debug.js
@@ -0,0 +1,26 @@
+"use strict";
+
+window.debug = window.location.hash == "#debug";
+
+function debugInit() {
+  function debugPlaying() {
+    document.body.classList.remove("attract");
+    document.body.classList.add("playing", "up");
+    $("#time-remaining-track").style.strokeDashoffset = 1482.83;
+  }
+
+  document.body.insertAdjacentHTML("beforeend", `
+    <div id="debug-root" style="position: absolute; top: 0; left: 0;">
+      <a href="#" id="debug-playing">Debug Playing</a>
+    </div>
+  `);
+
+  document.body.addEventListener("click", function(e) {
+    if(e.target.matches("#debug-playing")) {
+      e.preventDefault();
+      debugPlaying();
+    }
+  });
+}
+
+if(window.debug) document.addEventListener("DOMContentLoaded", debugInit);
