e74f1a0e30ad11b551fbc1ba13fc91797eedb906
Brenton "B-Train" Fletcher <i@bloople.net>
2018-11-13 02:48:10 +1100

    Add background images, for the moment just show one of them

--- js/panels/attribution.js
+++ js/panels/attribution.js
@@ -3,9 +3,14 @@
 window.Panels.Attribution = function() {
   function mount() {
     return `
-      Music: "<a href="http://freemusicarchive.org/music/Audiobinger/~/City_Lights_1859">City Lights</a>" by <a href="http://freemusicarchive.org/music/Audiobinger/">Audiobinger</a><br>
-      From the <a href="https://freemusicarchive.org/">Free Music Archive</a><br>
-      <a href="http://creativecommons.org/licenses/by-nc/4.0/">CC BY NC</a>
+      <h4>Music</h4>
+      <p>
+        "<a href="http://freemusicarchive.org/music/Audiobinger/~/City_Lights_1859">City Lights</a>" by <a href="http://freemusicarchive.org/music/Audiobinger/">Audiobinger</a><br>
+        From the <a href="https://freemusicarchive.org/">Free Music Archive</a><br>
+        <a href="http://creativecommons.org/licenses/by-nc/4.0/">CC BY NC</a>
+      </p>
+      <h4>Background image</h4>
+      <p>${Backgrounds.attribution()}</p>
     `;
   }
 
