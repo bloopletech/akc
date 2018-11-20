1fa90ebddba04a23e9542cf378e59606621867d9
Brenton "B-Train" Fletcher <i@bloople.net>
2018-11-20 13:13:16 +1100

    Only precache main app file

---
+++ sw-precache-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // Generate service-worker.js in the built version of the site
+  "root": "dist/", 
+  // Only precache the main app file (HTML + CSS + JS)
+  "staticFileGlobs": [
+    "dist/index.html"
+  ],
+  // Runtime cache the images and music
+  "runtimeCaching": [{
+    "urlPattern": /\.(?:png|gif|jpg|jpeg|svg|mp3)$/,
+    "handler": 'cacheFirst'
+  }]
+};
