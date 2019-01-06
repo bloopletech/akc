651163be086b3e85d584311f68f89361d4f482c8
Brenton "B-Train" Fletcher <i@bloople.net>
2019-01-06 18:04:42 +1100

    Re-implement grind; Adjust when time remaining bonus applies 0.3 -> 0.2, 0.15 -> 0.1; Remove reaction time bonus; Adjust allowed time reduction from every 20,000 score to 25,000 score

--- akc/js/engine.js
+++ akc/js/engine.js
@@ -4,6 +4,7 @@ window.engine = function() {
   var CODES_MAP = { 37: "left", 38: "up", 39: "right", 40: "down", 65: "left", 87: "up", 68: "right", 83: "down" };
 
   window.$ = document.querySelector.bind(document);
+  var $grindRatio = $("#grind-ratio");
   var $stack = $("#stack");
   var $stackTrack = $("#stack-track");
   var $timeRemainingTrack = $("#time-remaining-track");
@@ -12,6 +13,7 @@ window.engine = function() {
 
   var highPrecisionTimer = (typeof window.performance == "object");
   var game = null;
+  var currentCode = null;
   var state = null;
   var timeUsedUpdater = null;
 
@@ -48,21 +50,64 @@ window.engine = function() {
     $stack.style.strokeDasharray = ((1262.92 / game.maxStacks()) - 4) + " 4";
     $stackTrack.style.strokeDashoffset = ((game.stack() + 1) / game.maxStacks()) * 1262.92;
     showDirection(game.roundStarted(timeNow()));
+    currentCode = null;
   }
 
   function updateTimeUsed() {
     var now = timeNow();
     timeUsedUpdater = window.requestAnimationFrame(updateTimeUsed);
 
+    var grindRatio = game.grindRatio(now);
+    $grindRatio.style.r = grindRatio > 0 ? (grindRatio * 184) : 0;
+
     $timeRemainingTrack.style.strokeDashoffset = (1482.83 - (game.timeRemainingRatio(now) * 1482.83));
+
+    if(game.grinding()) document.body.classList.add("grinding");
+    else document.body.classList.remove("grinding");
 
     var newScore = game.score() + game.delta(now);
     $score.textContent = newScore > 0 ? newScore.toLocaleString() : "";
 
-    if(game.timeRemaining(now) < 0) setTimeout(endRound, 0, false, now);
+    if(game.timeRemaining(now) < 0) setTimeout(onTimeUsed, 0, now);
   }
 
+  function onTimeUsed(now) {
+    if(!game.canCombo()) {
+      endRound(now);
+      return;
+    }
+
+    var code = currentCode;
+    endRound(game.finishTime());
+    currentCode = code;
+
+    game.input(code);
+    game.grindStarted(now);
+    game.comboed();
+   }
+
   function onInputStart(event, code) {
+    if(document.body.classList.contains("panel-open")) return;
+
+    if(state != "playing") return;
+    event.preventDefault();
+
+    if(!currentCode) {
+      if(!game.input(code)) {
+        endRound(event.timeStamp);
+        return;
+      }
+
+      currentCode = code;
+      game.grindStarted(event.timeStamp);
+    }
+    else if(currentCode != code) {
+      game.input();
+      endRound(event.timeStamp);
+    }
+  }
+
+  function onInputEnd(event, code) {
     if(document.body.classList.contains("panel-open")) return true;
 
     if(state == "waiting") {
@@ -73,12 +118,16 @@ window.engine = function() {
     if(state != "playing") return false;
     event.preventDefault();
 
-    flash();
-    endRound(code, event.timeStamp);
+    if(!currentCode || currentCode != code) game.input();
+    endRound(event.timeStamp);
   }
 
   function onKeyDown(event) {
-    if(onInputStart(event, CODES_MAP[event.keyCode])) return;
+    onInputStart(event, CODES_MAP[event.keyCode]);
+  }
+
+  function onKeyUp(event) {
+    if(onInputEnd(event, CODES_MAP[event.keyCode])) return;
 
     if(event.keyCode == 32 || event.keyCode == 13) {
       event.preventDefault();
@@ -90,20 +139,22 @@ window.engine = function() {
     onInputStart(event, event.target.dataset.direction);
   }
 
-  function flash() {
-    $("#flasher").classList.add("animated");
-    var $flasher = $("#flasher").cloneNode();
-    $("#time-remaining").replaceChild($flasher, $("#flasher"));
+  function onTouchEnd(event) {
+    onInputEnd(event, event.target.dataset.direction);
   }
 
-  function endRound(code, now) {
-    var outcome = game.roundEnded(code, now);
+  function endRound(now) {
+    currentCode = null;
+
+    var outcome = game.roundEnded(now);
     if(outcome) gameOver(outcome);
     else startRound(now);
   }
 
   function resetPlayField() {
     showDirection("blank");
+    document.body.classList.remove("grinding");
+    $grindRatio.style.r = 0;
     $score.textContent = "";
     $timeRemainingTrack.style.strokeDashoffset = 0;
     $stackTrack.style.strokeDashoffset = 0;
@@ -114,7 +165,6 @@ window.engine = function() {
 
     rejectTouchPlay();
     resetPlayField();
-    $("#flasher").classList.remove("animated");
     $("#results-score").textContent = game.score().toLocaleString();
     $("#results-rank").textContent = Ranks.scoreRank(game.score()).humanName;
     $("#results-streak").textContent = game.streak().toLocaleString();
@@ -138,13 +188,16 @@ window.engine = function() {
     document.body.classList.add("touch");
 
     window.removeEventListener("keydown", onKeyDown);
+    window.removeEventListener("keyup", onKeyUp);
     window.addEventListener("touchstart", onTouchStart);
+    window.addEventListener("touchend", onTouchEnd);
     $(".play").removeEventListener("touchstart", touchDevice);
   }
 
   $(".play").addEventListener("touchstart", touchDevice);
 
   window.addEventListener("keydown", onKeyDown);
+  window.addEventListener("keyup", onKeyUp);
 
   document.body.addEventListener("click", function(e) {
     if(e.target.matches(".play:not(.disabled)")) start();
--- akc/js/game.js
+++ akc/js/game.js
@@ -1,18 +1,38 @@
 "use strict";
 
 window.Game = function(touch) {
-  var initialAllowedTime = 1500;
+  var initialAllowedTime = 1200;
   var allowedTime = initialAllowedTime;
   var score = 0;
   var streak = 1;
   var startTime = null;
+  var grindStart = null;
+  var combo = null;
   var direction = null;
   var pattern = new Pattern();
+  var correct = false;
   var roundLogs = [];
 
+  function grindTime(now) {
+    return ((now - grindStart) * 2);
+  }
+
   function timePassed(now) {
-    var diff = now - startTime;
-    return diff * ((diff / allowedTime) + 1);
+    if(grindStart) {
+      return grindTime(now) + (grindStart - startTime);
+    }
+    else {
+      return now - startTime;
+    }
+  }
+
+  function finishTime() {
+    if(grindStart) {
+      return grindStart + (((startTime + allowedTime) - grindStart) / 2.0);
+    }
+    else {
+      return startTime + allowedTime;
+    }
   }
 
   function timeRemaining(now) {
@@ -29,13 +49,53 @@ window.Game = function(touch) {
   function roundStarted(now) {
     direction = pattern.next();
     startTime = now;
+    correct = false;
+    grindStart = null;
+    combo = null;
     return direction;
   }
 
+  function input(playerDirection) {
+    correct = playerDirection == direction;
+    return correct;
+  }
+
+  function grindStarted(now) {
+    grindStart = now;
+  }
+
+  function grindDuration(now) {
+    return grindStart ? (now - grindStart) : 0;
+  }
+
+  function grindRatio(now) {
+    if(!grindStart) return 0;
+    var ratio = grindTime(now) / (allowedTime - (grindStart - startTime));
+    if(ratio < 0) return 0;
+    if(ratio > 1) return 1;
+    return ratio;
+  }
+
+  function reactionTime() {
+    return grindStart - startTime;
+  }
+
+  function canCombo() {
+    return correct && (direction == pattern.peek());
+  }
+
+  function comboed() {
+    combo = true;
+  }
+
   function delta(now) {
-    var delta = (initialAllowedTime - (now - startTime)) * Math.max(1, streak / 20);
-    if(timeRemainingRatio(now) <= 0.15) delta *= 3;
-    else if(timeRemainingRatio(now) <= 0.3) delta *= -2;
+    if(!grindStart) return 0;
+
+    var delta = ((initialAllowedTime - reactionTime()) + grindDuration(now)) * Math.max(1, streak / 20);
+    if(timeRemainingRatio(now) <= 0.1) delta *= 2;
+    else if(timeRemainingRatio(now) <= 0.2) delta *= 1.5;
+
+    if(combo) delta *= 2;
     return Math.floor(delta);
   }
 
@@ -45,21 +105,22 @@ window.Game = function(touch) {
       score: score,
       streak: streak,
       startTime: startTime,
+      grindStart: grindStart,
+      combo: combo,
       direction: direction,
       now: now,
       diff: timePassed(now)
     });
   }
 
-  function roundEnded(playerDirection, now) {
+  function roundEnded(now) {
     if(timePassed(now) > allowedTime) return "timeExceeded";
-    if(playerDirection != direction) return "incorrect";
+    if(!correct) return "incorrect";
 
     score += delta(now);
-    if(score < 0) score = 0;
     createLogEntry(now);
 
-    allowedTime = Math.max(initialAllowedTime - (75 * Math.floor(score / 20000)), 250);
+    allowedTime = Math.max(initialAllowedTime - (75 * Math.floor(score / 25000)), 250);
 
     streak++;
   }
@@ -67,6 +128,9 @@ window.Game = function(touch) {
   return {
     touch: function() {
       return touch;
+    },
+    grinding: function() {
+      return !!grindStart;
     },
     score: function() {
       return score;
@@ -84,10 +148,16 @@ window.Game = function(touch) {
       return roundLogs;
     },
     roundStarted: roundStarted,
+    input: input,
     timeRemaining: timeRemaining,
     timeRemainingRatio: timeRemainingRatio,
+    grindStarted: grindStarted,
+    finishTime: finishTime,
+    canCombo: canCombo,
+    comboed: comboed,
     roundEnded: roundEnded,
-    delta: delta
+    delta: delta,
+    grindRatio: grindRatio
   };
 };
 
