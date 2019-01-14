75b8492093f76d29fb12f590d7e79e4262a7617d
Brenton "B-Train" Fletcher <i@bloople.net>
2019-01-14 20:19:20 +1100

    Refactor service worker

--- akc/service-worker.js
+++ akc/service-worker.js
@@ -19,52 +19,38 @@
 var precacheConfig = [["index.html","index_html_hash"]];
 var cacheName = 'sw-precache-v3-sw-precache-' + (self.registration ? self.registration.scope : '');
 
-var addDirectoryIndex = function(originalUrl, index) {
-    var url = new URL(originalUrl);
-    if(url.pathname.slice(-1) === '/') {
-      url.pathname += index;
-    }
-    return url.toString();
-  };
-
 var cleanResponse = function(originalResponse) {
-    // If this is not a redirected response, then we don't have to do anything.
-    if(!originalResponse.redirected) {
-      return Promise.resolve(originalResponse);
-    }
+  // If this is not a redirected response, then we don't have to do anything.
+  if(!originalResponse.redirected) {
+    return Promise.resolve(originalResponse);
+  }
 
-    // Firefox 50 and below doesn't support the Response.body stream, so we may
-    // need to read the entire body to memory as a Blob.
-    var bodyPromise = 'body' in originalResponse ?
-      Promise.resolve(originalResponse.body) :
-      originalResponse.blob();
+  // Firefox 50 and below doesn't support the Response.body stream, so we may
+  // need to read the entire body to memory as a Blob.
+  var bodyPromise = 'body' in originalResponse ?
+    Promise.resolve(originalResponse.body) :
+    originalResponse.blob();
 
-    return bodyPromise.then(function(body) {
-      // new Response() is happy when passed either a stream or a Blob.
-      return new Response(body, {
-        headers: originalResponse.headers,
-        status: originalResponse.status,
-        statusText: originalResponse.statusText
-      });
+  return bodyPromise.then(function(body) {
+    // new Response() is happy when passed either a stream or a Blob.
+    return new Response(body, {
+      headers: originalResponse.headers,
+      status: originalResponse.status,
+      statusText: originalResponse.statusText
     });
-  };
-
-var createCacheKey = function(originalUrl, paramValue) {
-    // Create a new URL object to avoid modifying originalUrl.
-    var url = new URL(originalUrl);
-
-    url.search += (url.search ? '&' : '') + 'precache=' + encodeURIComponent(paramValue);
-
-    return url.toString();
-  };
+  });
+};
 
 var urlsToCacheKeys = new Map(
   precacheConfig.map(function(item) {
     var relativeUrl = item[0];
     var hash = item[1];
     var absoluteUrl = new URL(relativeUrl, self.location);
-    var cacheKey = createCacheKey(absoluteUrl, hash);
-    return [absoluteUrl.toString(), cacheKey];
+
+    var cachedUrl = new URL(absoluteUrl);
+    cachedUrl.search += (cachedUrl.search ? '&' : '') + 'precache=' + encodeURIComponent(hash);
+
+    return [absoluteUrl.toString(), cachedUrl.toString()];
   })
 );
 
@@ -132,46 +118,34 @@ self.addEventListener('activate', function(event) {
 
 
 self.addEventListener('fetch', function(event) {
-  if(event.request.method === 'GET') {
-    // Should we call event.respondWith() inside this fetch event handler?
-    // This needs to be determined synchronously, which will give other fetch
-    // handlers a chance to handle the request if need be.
-    var shouldRespond;
+  if(event.request.method != 'GET') return;
 
-    // First, remove any hash fragment, and see if we
-    // have that URL in our cache. If so, great! shouldRespond will be true.
-    var url = new URL(event.request.url);
-    // Remove the hash; see https://github.com/GoogleChrome/sw-precache/issues/290
-    url.hash = '';
-    url = url.toString();
-    shouldRespond = urlsToCacheKeys.has(url);
+  // First, remove any hash fragment, and see if we
+  // have that URL in our cache. If so, great!
+  var url = new URL(event.request.url);
 
-    // If shouldRespond is false, check again, this time with 'index.html'
-    // (or whatever the directoryIndex option is set to) at the end.
-    var directoryIndex = 'index.html';
-    if(!shouldRespond && directoryIndex) {
-      url = addDirectoryIndex(url, directoryIndex);
-      shouldRespond = urlsToCacheKeys.has(url);
-    }
+  // Remove the hash; see https://github.com/GoogleChrome/sw-precache/issues/290
+  url.hash = '';
+  // add 'index.html' at the end if path refers to a directory.
+  if(url.pathname.slice(-1) === '/') url.pathname += 'index.html';
 
-    // If shouldRespond was set to true at any point, then call
-    // event.respondWith(), using the appropriate cache key.
-    if(shouldRespond) {
-      event.respondWith(
-        caches.open(cacheName).then(function(cache) {
-          return cache.match(urlsToCacheKeys.get(url)).then(function(response) {
-            if(response) {
-              return response;
-            }
-            throw Error('The cached response that was expected is missing.');
-          });
-        }).catch(function(e) {
-          // Fall back to just fetch()ing the request if some unexpected error
-          // prevented the cached response from being valid.
-          console.warn('Couldn\'t serve response for "%s" from cache: %O', event.request.url, e);
-          return fetch(event.request);
-        })
-      );
-    }
+  url = url.toString();
+
+  if(urlsToCacheKeys.has(url)) {
+    event.respondWith(
+      caches.open(cacheName).then(function(cache) {
+        return cache.match(urlsToCacheKeys.get(url)).then(function(response) {
+          if(response) {
+            return response;
+          }
+          throw Error('The cached response that was expected is missing.');
+        });
+      }).catch(function(e) {
+        // Fall back to just fetch()ing the request if some unexpected error
+        // prevented the cached response from being valid.
+        console.warn('Couldn\'t serve response for "%s" from cache: %O', event.request.url, e);
+        return fetch(event.request);
+      })
+    );
   }
 });
